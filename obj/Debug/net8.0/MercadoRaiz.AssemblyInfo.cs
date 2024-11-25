812575976ebd14d434bcad5369b32eecd333947b
Kayky <kayky159@gmail.com>
2024-11-24 23:49:08 -0300

    STYLE : Cadastrar Produto e Gerenciar Produtos

--- obj/Debug/net8.0/MercadoRaiz.AssemblyInfo.cs
+++ obj/Debug/net8.0/MercadoRaiz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MercadoRaiz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e934c6b22fcadc644786696ad4fe6ea73ac6f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c58b8c939e2837819e6af2804f77e010e60e3a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MercadoRaiz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MercadoRaiz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
